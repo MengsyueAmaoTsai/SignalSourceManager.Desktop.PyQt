eb5e19139c7bcf26cc74f39e69662aeb653b03da
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-10-14 20:37:46 +0800

    Remove unused imports

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import sys
 
 from PySide6.QtCore import QProcess
 from PySide6.QtGui import QGuiApplication
-from PySide6.QtQml import QQmlApplicationEngine, qmlRegisterSingletonType
+from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtQuick import QQuickWindow, QSGRendererInterface
 from PySide6.QtQuickControls2 import QQuickStyle
 
